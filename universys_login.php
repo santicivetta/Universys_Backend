7d83881c0f12b10de95ec6ee4ccd39ce7382df59
Unknown <santiago.civettam@gmail.com>
2017-10-14 11:46:55 -0300

    algunos avances en login.ph

--- universys_login.php
+++ universys_login.php
@@ -1,59 +1,115 @@
+
+/*
+id sesion
+user agent
+usuario
+*/
+-----------------Login:
+Request de Login:
+Dirección URL del Servidor: http://universys.site/login
+Ejemplo de JSON:
+{
+	“apiVer” : ”1.0”,
+	“idSesion” : “32695”
+	“mail” : ”pepito@gmail.com”,
+	“password” : “pepitokpo” 
+}
+
+Respuesta del Login:
+Ejemplo de JSON:
+{
+	“direccionServidor” : “x”
+	“apiVer” : ”1.0”,
+	“errorId” : “404”,
+	“usuario” : { 
+				“nombre” : ”Diego”,
+				“apellido” : “Maradona”,
+				“fNac” : “3/8/01”
+				“tipo” : “alumno”
+				}
+}
+
+Codigo de error: 200:”Página funcionando correctamente”.
+Codigo de error: 680:”Usuario o mail incorrecto”
+Codigo de error: 777:”Contraseña incorrecta”
+Codigo de error: 799:”Error: Sesión duplicada”
+Codigo de error: 800:”Error: Unexpected error”
+Codigo de error: 801:”Invalid JSON request”
+Codigo de error: 802:”Unable to connect to database”
+
 <?php  
 
-function isValidJSON($str) {
-	json_decode($str);
-	return json_last_error() == JSON_ERROR_NONE;
+ public static function encode($value, $options = 0) {
+	$result = json_encode($value, $options);
+
+	if($result)  {
+		return $result;
+	}
+
+	throw new Exception('800');;
+	}
+
+public static function decode($json, $assoc = false) {
+    $result = json_decode($json, $assoc);
+
+    if($result) {
+        return $result;
+    }
+
+    throw new Exception('800');
 }
 
-$connection = new mysqli('universys.site', 'apholos_dba', 'dbainub', 'apholos_ligaub');
+public static function connect(){
+	$connection = new mysqli('universys.site', 'apholos_dba', 'dbainub', 'apholos_ligaub');
 
+	if ($mysqli->connect_errno) {
+	    throw new Exception('802');
+	}
 
-if (mysqli_connect_errno()) {
-	die("Database connection failed: " .
-		mysqli_connect_error() .
-		" (" . mysqli_connect_errno() . ")"
-	);
+	return $connection;
 }
+
+
+
+
+
+
 
 $json_params = file_get_contents("php://input");
 
-if (strlen($json_params) > 0 && isValidJSON($json_params)){
+$request = decode($json_params);
 
-	$decoded_json = json_decode($json_params);
-	$mail = $decoded_json["mail"];
-	$password = $decoded_json["password"];
+$mail = $request["mail"];
+$password = $request["password"];
 
-	$query = "SELECT * FROM usuarios WHERE mail='$mail' and password='$password'";
-	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-	$count = mysqli_num_rows($result);
+$query = "SELECT * FROM usuarios WHERE mail='$mail' and password='$password'";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
 
-	if ($count == 1){
-		if ($result = $mysqli->query("SELECT * FROM phase1")) {
+if ($count == 1){
+	if ($result = $mysqli->query("SELECT * FROM phase1")) {
 
-			while($row = $result->fetch_array(MYSQL_ASSOC)) {
-				$myArray[] = $row;
-			}
-			echo json_encode($myArray);
+		while($row = $result->fetch_array(MYSQL_ASSOC)) {
+			$myArray[] = $row;
 		}
-	}else{
-
-		echo "<script language='javascript'>
-		alert('ERROR: Credenciales invalidas');
-		window.location.href = 'mail.html';
-		</script>";
+		echo json_encode($myArray);
 	}
+}else{
+
+	echo "<script language='javascript'>
+	alert('ERROR: Credenciales invalidas');
+	window.location.href = 'mail.html';
+	</script>";
+}
 }
 ?>
 
 /*pasar todo lo de arriba a objetos*/
 <?php
-$mysqli = new mysqli("localhost", "my_user", "my_password", "world");
+
+
 
 /* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
 
 /* Create table doesn't return a resultset */
 if ($mysqli->query("CREATE TEMPORARY TABLE myCity LIKE City") === TRUE) {
