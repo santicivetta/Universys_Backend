1a98bffe6e39685977d71b3aa3a1ff527fb38d6a
Mariano Martin <mariano.d.martin@gmail.com>
2017-10-19 17:27:58 -0300

    Se agregan roles, profesores y administradores

--- bases.sql
+++ bases.sql
@@ -3,14 +3,19 @@ use apholos_universys;
 CREATE TABLE Usuarios(
 usuario varchar(50) not null unique,
 contraseña varchar(30) not null,
-id_rol int not null,
-PRIMARY KEY(mail));
+idRol int not null,
+PRIMARY KEY(mail),
+FOREIGN KEY(id_rol) REFERENCES Roles(id_rol));
 
 CREATE TABLE Alumnos(
 matricula int not null unique,
+documento int not null,
 nombre varchar(30) not null,
 apellido varchar(30) not null,
 mail varchar(50) not null unique,
+fechaNacimiento date,
+genero varchar(20),
+domicilio varchar(100),
 PRIMARY KEY (matricula),
 FOREIGN KEY (mail) REFERENCES Usuarios(usuario));
 
@@ -20,9 +25,35 @@ usuario varchar(50) not null,
 PRIMARY KEY(idSesion),
 FOREIGN KEY (usuario) REFERENCES Usuarios(usuario));
 
+CREATE TABLE Roles(
+idRol int not null unique,
+descripcion varchar(30) not null,
+tabla varchar(30) not null,
+PRIMARY KEY (id_rol));
 
+CREATE TABLE Profesores(
+legajo int not null unique,
+documento int not null,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+mail varchar(50) not null unique,
+fechaNacimiento date,
+genero varchar(20),
+domicilio varchar(100),
+PRIMARY KEY (legajo),
+FOREIGN KEY (mail) REFERENCES Usuarios(usuario));
 
-
+CREATE TABLE Administradores(
+legajo int not null unique,
+documento int not null,
+nombre varchar(30) not null,
+apellido varchar(30) not null,
+mail varchar(50) not null unique,
+fechaNacimiento date,
+genero varchar(20),
+domicilio varchar(100),
+PRIMARY KEY (legajo),
+FOREIGN KEY (mail) REFERENCES Usuarios(usuario));
 
 
 
