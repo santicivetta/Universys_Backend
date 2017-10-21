0ecd094a7f9f11187e2e5ec444d0e90b9a04a960
Unknown <santiago.civettam@gmail.com>
2017-10-20 21:55:32 -0300

    mas avances

--- universys_login.php
+++ universys_login.php
@@ -1,10 +1,5 @@
-//que hacemos con varios errores?
 
-/*
-id sesion
-user agent
-usuario
-*/
+
 -----------------Login:
 Request de Login:
 Dirección URL del Servidor: http://universys.site/login
@@ -25,7 +20,7 @@ Ejemplo de JSON:
 	“usuario” : { 
 				“nombre” : ”Diego”,
 				“apellido” : “Maradona”,
-				“fNac” : “3/8/01”
+				“fNac” : “3/8/01”,
 				“tipo” : “alumno”
 				}
 }
@@ -47,11 +42,12 @@ Codigo de error: 802:”Unable to connect to database”
 		return $result;
 	}
 
-	throw new Exception('800');;
+	throw new Exception('800');
+
 	}
 
-public static function decode($json, $assoc = false) {
-    $result = json_decode($json, $assoc);
+public static function decode($json) {
+    $result = json_decode($json);
 
     if($result) {
         return $result;
@@ -70,25 +66,46 @@ public static function connect(){
 	return $connection;
 }
 
-public static function chequeoVersion($connection, $versionAChequear){
+public static function chequeoVersion($conexion, $versionAChequear){
+
+	$result = $conexion->query("SELECT version FROM api_version WHERE fecha_hasta IS NULL");
+
+	if ($conexion->connect_errno) {
+	    throw new Exception('802');
+	}
+
+	if ($result->num_rows == 1) {
+		$row = $result->fetch_array(MYSQL_ASSOC);
+		return ($row["version"] == $versionAChequear);
+	} else {
+		throw new Exception('1500');
+	}
 
 }
 
-public static function validoCredenciales($conexion, $mail, $password){
-	$query = "SELECT * FROM usuarios WHERE mail='$mail' and password='$password'";
-	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-	$count = mysqli_num_rows($result);
+public static function validoCredenciales($conexion, $usuario, $contraseña){
+	
+	$result = $conexion->query("SELECT * FROM Usuarios WHERE usuario = " . $usuario . " and contraseña = " . $contraseña);
+
+	if ($conexion->connect_errno) {
+	    throw new Exception('802');
+	}
+
+	if ($result->num_rows == 1) {
+		$row = $result->fetch_array(MYSQL_ASSOC);
+		return $row["usuario"];
+	} else {
+		throw new Exception('680');
+	}
 
 }
 
 public static function traigoDatos($conexion, $idUsuario){
-	if ($result = $mysqli->query("SELECT * FROM phase1")) {
-
+	if ($result = $conexion->query("SELECT * FROM Usuarios where usuario=" . $idUsuario)) {
 		while($row = $result->fetch_array(MYSQL_ASSOC)) {
 			$myArray[] = $row;
 		}
 }
-
 
 
 try {
@@ -100,11 +117,9 @@ try {
 	$miJson = decode($json_params);
 
 	//chequeo id sesion
-	if (isset($request["sesion"])) {
+	if (isset($miJson["sesion"])) {
 		throw new Exception("799");	
 	}
-
-	//chequeo user agent ver si sirve http://mobiledetect.net
 
 	//conecto a la base
 	$conexion = connect();
@@ -128,10 +143,9 @@ try {
 	$arraySalida = armarSalida(null, $e->getMessage());
 
 	$mysqli->close();
-	
+
 	echo json_encode($arraySalida);
 
 }
-
 
 ?>
