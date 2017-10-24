bd421ac12df90bf84142707c606dc3fb813e55c5
Mariano Martin <mariano.d.martin@gmail.com>
2017-10-24 20:45:39 -0300

    Se agrega archivo de defines

---
+++ defines.php
@@ -0,0 +1,9 @@
+<?php
+//******************************
+//*******CODIGOS DE ERROR*******
+//******************************
+
+define('apiNoCompatible', '1500');
+define('sesionInexistente', '1501');
+define('errorConexion','802');
+?>
