b220612da117cdc8a5efeff5346f6a0894c9432f
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-10 20:31:38 -0300

    Se realizan modificaciones a pruebaLogout.php

--- testing/testingLogout/pruebaLogout.php
+++ testing/testingLogout/pruebaLogout.php
@@ -3,7 +3,7 @@ define('TESTING', 'true');
 include_once (__DIR__ . '/../funcionesTesting.php');
 include_once (__DIR__ . '/../../funcionesGenerales.php');
 include_once (__DIR__ . '/../../defines.php');
-include_once ('../../universys_login.php');
+include_once ('../../logout.php');
 
 $eCon=null;
 try{
@@ -14,12 +14,12 @@ catch (Exception $eCon) {
     echo 'Excepción capturada: ',  $eCon->getMessage(), "\n";
 }
 if($eCon==null){
-  echo ">> <strong>Login</strong> </br>";
+  echo ">> <strong>Logout</strong> </br>";
 
   //PRUEBA 1
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "santiago.civetta@comunidad.ub.edu.ar", "password" => "contraseñasantiago"]);
+  $datos= doLogout(["apiVer" => apiVersionActual, "idSesion" => "1"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
+  if($datosParseados["errorId"]=='200'){
     echo "Prueba1 OK </br>";
   }
   else{
@@ -27,35 +27,16 @@ if($eCon==null){
   }
   
   //PRUEBA 2
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "mariano.martin@comunidad.ub.edu.ar", "password" => "contraseñasarasa"]);
+  $datos= doLogout(["apiVer" => apiVersionActual, "idSesion" => "10"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==credencialesIncorrectas){
+  if($datosParseados["errorId"]==sesionInexistente){
     echo "Prueba2 OK </br>";
   }
   else{
     echo "Prueba2 FAIL </br>";
   }
-
-  //PRUEBA 3
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "gaston.bodeman@comunidad.ub.edu.ar", "password" => "contraseñagaston"]);
-  $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==personaInexistente){
-    echo "Prueba3 OK </br>";
-  }
-  else{
-    echo "Prueba3 FAIL </br>";
-  }
-
-  //PRUEBA 4
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "andres.didier@comunidad.ub.edu.ar", "password" => "contraseñaandres"]);
-  $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
-    echo "Prueba4 OK </br>";
-  }
-  else{
-    echo "Prueba4 FAIL </br>";
-  }
 }
+
 mysqli_close($conexion);
 
 ?>
