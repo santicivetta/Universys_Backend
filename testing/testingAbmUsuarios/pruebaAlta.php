b4a29cbc84e7c3eec623725ca719e1c37fd7d8c4
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-11 15:53:55 -0300

    Se copia archivo de testing como pruebaAlta.php

---
+++ testing/testingAbmUsuarios/pruebaAlta.php
@@ -0,0 +1,69 @@
+<?php
+define('TESTING', 'true');
+include_once (__DIR__ . '/../funcionesTesting.php');
+include_once (__DIR__ . '/../../funcionesGenerales.php');
+include_once (__DIR__ . '/../../defines.php');
+include_once ('../../login.php');
+
+$eCon=null;
+try{
+$conexion = connect(True);
+ejecutarSQL('login.sql',$conexion);
+} 
+catch (Exception $eCon) {
+    echo 'Excepción capturada: ',  $eCon->getMessage(), "\n";
+}
+if($eCon==null){
+  echo ">> <strong>Login</strong> </br>";
+
+  //PRUEBA 1
+  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "santiago.civetta@comunidad.ub.edu.ar", "password" => "contraseñasantiago"]);
+  $datosParseados= json_decode($datos, true);
+  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
+    echo "Prueba1 OK </br>";
+  }
+  else{
+    echo "Prueba1 FAIL </br>";
+  }
+  
+  //PRUEBA 2
+  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "mariano.martin@comunidad.ub.edu.ar", "password" => "contraseñasarasa"]);
+  $datosParseados= json_decode($datos, true);
+  if($datosParseados["errorId"]==credencialesIncorrectas){
+    echo "Prueba2 OK </br>";
+  }
+  else{
+    echo "Prueba2 FAIL </br>";
+  }
+
+  //PRUEBA 3
+  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "gaston.bodeman@comunidad.ub.edu.ar", "password" => "contraseñagaston"]);
+  $datosParseados= json_decode($datos, true);
+  if($datosParseados["errorId"]==personaInexistente){
+    echo "Prueba3 OK </br>";
+  }
+  else{
+    echo "Prueba3 FAIL </br>";
+  }
+
+  //PRUEBA 4
+  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "andres.didier@comunidad.ub.edu.ar", "password" => "contraseñaandres"]);
+  $datosParseados= json_decode($datos, true);
+  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
+    echo "Prueba4 OK </br>";
+  }
+  else{
+    echo "Prueba4 FAIL </br>";
+  }
+}
+mysqli_close($conexion);
+
+?>
+
+
+
+
+
+
+
+
