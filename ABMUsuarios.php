2787c672bebe03ebc2769d2bff65ff1a382223e5
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-11 18:57:45 -0300

    se elimina funcion innecesaria

--- ABMUsuarios.php
+++ ABMUsuarios.php
@@ -4,41 +4,6 @@
 include_once ('defines.php');
 include_once ('funcionesGenerales.php');
 
-function traerDatos($conexion, $idUsuario){
-
-	$query="SELECT u.usuario,r.tabla, r.descripcion
-			FROM 	Usuarios u,
-					Roles r
-			WHERE 	u.idRol=r.idRol
-			and u.usuario = '" .$idUsuario."'";
-
-	$result = $conexion->query($query);
-
-	if ($conexion->connect_errno)
-	    throw new Exception(errorConexionBase);
-
-
-	if ($result->num_rows != 1)
-		throw new Exception(sesionInexistente);
-
-	$row = $result->fetch_array(MYSQLI_ASSOC);
-
-	$query2 = "SELECT nombre, apellido FROM " . $row["tabla"] . " where mail = '" . $row["usuario"] . "'";
-
-	$result2 = $conexion->query($query2);
-
-	if ($conexion->connect_errno) {
-		throw new Exception(errorConexionBase);
-	}
-
-	if ($result2->num_rows != 1)
-		throw new Exception(sesionInexistente);
-
-	$row2 = $result2->fetch_array(MYSQLI_ASSOC);
-
-	return array_merge($row2, array("rol"=> $row["descripcion"]));
-}
-
 function doABMUsuarios($data) {
 	try {
 		if (!(isset($data))) {
