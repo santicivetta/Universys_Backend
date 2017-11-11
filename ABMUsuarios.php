48d7aeeda77bdbeabb3dff147edb5d1fd9ae4ec3
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-11 18:57:18 -0300

    Se eliminan bugs de sintaxis

--- ABMUsuarios.php
+++ ABMUsuarios.php
@@ -41,49 +41,44 @@ function traerDatos($conexion, $idUsuario){
 
 function doABMUsuarios($data) {
 	try {
-
-		if (!(isset($_POST))) {
+		if (!(isset($data))) {
 			throw new Exception(errorInesperado);	
 		}
 
-		if (!(empty($_POST["idSesion"]))) {
-			throw new Exception(sesionDuplicada);	
-		}
-
 		//chequeo version
-		VersionDeAPICorrecta($_POST["apiVer"]);
+		VersionDeAPICorrecta($data["apiVer"]);
 
 		//conecto a la base
 		$conexion = connect();
 
 		//valido sesion
-		$miUsuario = validarSesion($conexion, $_POST["idSesion"]);
+		$miUsuario = validarSesion($conexion, $data["idSesion"]);
 
 		//valido permisos
-		if strcmp($miUsuario["tabla"],"Administradores") != 0 {
+		if (strcmp($miUsuario["tabla"],"Administradores") != 0) {
 			throw new Exception(permisosErroneos);	
 		}
 
-		if strcmp($_POST["operacion"], "alta") {
+		if (strcmp($data["operacion"], "alta")==0) {
 
-			if strcmp($_POST["tabla"],"administrador") {
-				$query = "INSERT INTO Usuarios(usuario, contraseña, fechaAlta) values ('".$_POST["mail"]."','".$_POST["contrasena"]."',CURDATE())";
+			if (strcmp($data["rol"],"Administrador")==0) {
+				$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),3,null)";
 				
-				$query2 = "INSERT INTO Administradores(legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) values ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."')";
+				$query2 = "INSERT INTO Administradores(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
 
 			}
 
-			if strcmp($_POST["tabla"],"profesor") {
-				$query = "INSERT INTO Usuarios(usuario, contraseña, fechaAlta) values ('".$_POST["mail"]."','".$_POST["contrasena"]."',CURDATE())";
+			if (strcmp($data["rol"],"Profesor")==0) {
+				$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),2,null)";
 
-				$query2 = "INSERT INTO Profesores(legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) values ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."')";
+				$query2 = "INSERT INTO Profesores(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
 
 			}
 
-			if strcmp($_POST["tabla"],"alumno") {
-				$query = "INSERT INTO Usuarios(usuario, contraseña, fechaAlta) values ('".$_POST["mail"]."','".$_POST["contrasena"]."',CURDATE())";
+			if (strcmp($data["rol"],"Alumno")==0) {
+				$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),1,null)";
 
-				$query2 = "INSERT INTO Alumnos(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) values ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."')";
+				$query2 = "INSERT INTO Alumnos(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
 
 			}
 
@@ -95,48 +90,45 @@ function doABMUsuarios($data) {
 				throw new Exception(errorConexionBase);
 			};
 
-			$arraySalida = armarSalida(null, "200");
 		}
 
-		if strcmp($_POST["operacion"], "modificacion") {
+		if (strcmp($data["operacion"], "modificacion")==0) {
 
-			if strcmp($_POST["tabla"],"administrador") {
-				$query = "UPDATE Administradores SET (legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."') WHERE mail = '" . $_POST["mail"] . "'";
-
-			}
-
-			if strcmp($_POST["tabla"],"profesor") {
-				$query = "UPDATE Profesores SET (legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."')WHERE mail = '" . $_POST["mail"] . "'";
+			if (strcmp($miUsuario["tabla"],"administrador")) {
+				$query = "UPDATE Administradores SET (legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."') WHERE mail = '" . $data["mail"] . "'";
 
 			}
 
-			if strcmp($_POST["tabla"],"alumno") {
-				$query = "UPDATE Alumnos SET (matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$_POST["identificador"]."','".$_POST["documento"]."','".$_POST["nombre"]."','".$_POST["apellido"]."','".$_POST["mail"]."','".$_POST["fnac"]."','".$_POST["genero"]."','".$_POST["domicilio"]."')WHERE mail = '" . $_POST["mail"] . "'";
+			if (strcmp($miUsuario["tabla"],"profesor")) {
+				$query = "UPDATE Profesores SET (legajo, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."')WHERE mail = '" . $data["mail"] . "'";
+
+			}
+
+			if (strcmp($miUsuario["tabla"],"alumno")) {
+				$query = "UPDATE Alumnos SET (matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio) = ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."')WHERE mail = '" . $data["mail"] . "'";
 
 			}
 
 			if ($conexion->query($query) === FALSE) {
 				throw new Exception(errorConexionBase);
 			};
-
-			$arraySalida = armarSalida(null, "200");
 			
 		}
 
-		if strcmp($_POST["operacion"], "baja") {
+		if (strcmp($data["operacion"], "baja")==0) {
 
 			$query = '	UPDATE 
 						FROM Usuarios 
 						SET fechaHasta = curdate()
-						WHERE mail = "' . $_POST["mail"] . '"';
+						WHERE mail = "' . $data["mail"] . '"';
 
 			if ($conexion->query($query) === FALSE) {
 				throw new Exception(errorConexionBase);
 			};
-
-			$arraySalida = armarSalida(null, "200");
 			
 		}
+
+		$arraySalida = armarSalida(null, "200","/ABMUsuarios");
 
 		$conexion->close();
 
@@ -144,7 +136,7 @@ function doABMUsuarios($data) {
 
 	} catch (Exception $e) {
 
-		$arraySalida = armarSalida(null, $e->getMessage());
+		$arraySalida = armarSalida(null, $e->getMessage(),"/ABMUsuarios");
 
 		if (isset($conexion)) {
 			$conexion->close();
