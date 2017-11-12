1483233c620cdc80085f706a4238651fa7114d0f
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-12 12:24:52 -0300

    Se modifica .sql para realizar testing

--- testing/testingAbmUsuarios/alta.sql
+++ testing/testingAbmUsuarios/alta.sql
@@ -6,8 +6,19 @@ INSERT INTO Roles(descripcion,tabla) values
 
 TRUNCATE TABLE Usuarios;
 INSERT INTO Usuarios values
-("bedel.gato@comunidad.ub.edu.ar",md5("contraseñabedel"),3,null);
+("bedel.gato@comunidad.ub.edu.ar",md5("contraseñabedel"),3,null),
+("santiago.civetta@comunidad.ub.edu.ar",md5("contraseñasantiago"),1,curdate());
 
 TRUNCATE TABLE Sesiones;
 INSERT INTO Sesiones(usuario,fechaAlta) values
-("bedel.gato@comunidad.ub.edu.ar",CURDATE());
+("bedel.gato@comunidad.ub.edu.ar",CURDATE()),
+("santiago.civetta@comunidad.ub.edu.ar",curdate());
+
+
+TRUNCATE TABLE Administradores;
+
+TRUNCATE TABLE Profesores;
+
+TRUNCATE TABLE Alumnos;
+INSERT INTO Alumnos values
+("3141","32341231","Santiago","Civetta","santiago.civetta@comunidad.ub.edu.ar","1990-03-02","Masculino","cabildo 123","47134567");
