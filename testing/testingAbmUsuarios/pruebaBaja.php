01a27a992205e5ae243b6ec5f96e6f8e6b697678
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-12 16:24:48 -0300

    Se realizan 3 test para baja

--- testing/testingAbmUsuarios/pruebaBaja.php
+++ testing/testingAbmUsuarios/pruebaBaja.php
@@ -7,17 +7,18 @@ include_once ('../../ABMUsuarios.php');
 
 $eCon=null;
 try{
-$conexion = connect(True);
-ejecutarSQL('baja.sql',$conexion);
+  $conexion = connect(True);
+  ejecutarSQL('baja.sql',$conexion);
 } 
 catch (Exception $eCon) {
     echo 'Excepción capturada: ',  $eCon->getMessage(), "\n";
 }
+
 if($eCon==null){
-  echo ">> <strong>Modificacion Usuario</strong> </br>";
+  echo ">> <strong>Baja Usuario</strong> </br>";
 
   //PRUEBA 1
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "nombre" => "Gaston", "apellido" => "Bodeman","documento" => "37213234", "fnac" => "1994-09-20","genero" => "Masculino","domicilio" => "Blanco Encalada 4892","telefono" => "45228786","mail" => "gaston.bodeman@comunidad.ub.edu.ar","contraseña" => "contraseñagaston"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "mail" => "gaston.bodeman@comunidad.ub.edu.ar"]);
   $datosParseados= json_decode($datos, true);
   if($datosParseados["errorId"]==usuarioInexistente){
     echo "Prueba1 OK </br>";
@@ -27,7 +28,7 @@ if($eCon==null){
   }
 
   //PRUEBA 2
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "modificacion", "nombre" => "Santi", "apellido" => "Cive","documento" => "12345678", "fnac" => "1994-09-10","genero" => "Masculino","domicilio" => "Cabildo 321","telefono" => "123456789","mail" => "santiago.civetta@comunidad.ub.edu.ar","contraseña" => "contraseñanueva"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "mail" => "andres.didier@comunidad.ub.edu.ar"]);
   $datosParseados= json_decode($datos, true);
   if($datosParseados["errorId"]=='200'){
     echo "Prueba2 OK </br>";
@@ -37,25 +38,15 @@ if($eCon==null){
   }
 
   //PRUEBA 3
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "modificacion", "nombre" => "Andi", "apellido" => "Didi","documento" => "123", "fnac" => "2004-09-10","genero" => "Indefinido","domicilio" => "Pampa y la via","telefono" => "987654321","mail" => "andres.didier@comunidad.ub.edu.ar","contraseña" => "contraseñanueva"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "2", "operacion" => "baja", "mail" => "sarasa@comunidad.ub.edu.ar"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200'){
+  if($datosParseados["errorId"]==permisosErroneos){
     echo "Prueba3 OK </br>";
   }
   else{
     echo "Prueba3 FAIL </br>";
   }
 
-  //PRUEBA 4
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "2", "operacion" => "modificacion", "nombre" => "Andi", "apellido" => "Didi","documento" => "123", "fnac" => "2004-09-10","genero" => "Indefinido","domicilio" => "Pampa y la via","telefono" => "987654321","mail" => "andres.didier@comunidad.ub.edu.ar","contraseña" => "contraseñanueva"]);
-  $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==permisosErroneos){
-    echo "Prueba4 OK </br>";
-  }
-  else{
-    echo "Prueba4 FAIL </br>";
-  }
-  
 }
 mysqli_close($conexion);
 
