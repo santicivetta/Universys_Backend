37eeeb0394ffc02140db3bacca5743fddfe03baa
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-12 12:25:02 -0300

    Se agregan defines

--- defines.php
+++ defines.php
@@ -7,9 +7,9 @@ define('apiNoCompatible', '1500');
 define('sesionInexistente', '1501');
 define('errorConexionBase','802');
 define('diasSesiones','4');
-define('hostBase','universys.site');
-define('usuarioBase','apholos_dba');
-define('contraseñaBase','dbainub');
+define('hostBase','localhost');
+define('usuarioBase','root');
+define('contraseñaBase','');
 define('nombreBase','apholos_universys');
 define('sesionVencida','1502');
 define('credencialesIncorrectas','680');
@@ -22,4 +22,5 @@ define('permisosErroneos','1002');
 define('sesionDuplicada','799');
 define('usuarioInexistente','1503');
 define('personaInexistente','1504');
+define('usuarioDuplicado','1505');
 ?>
