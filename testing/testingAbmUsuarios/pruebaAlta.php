6cc351091cd1240cdfcf91c77b3ead6669d84198
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-12 12:25:56 -0300

    Se realizan 4 test unitarios para alta

--- testing/testingAbmUsuarios/pruebaAlta.php
+++ testing/testingAbmUsuarios/pruebaAlta.php
@@ -3,33 +3,33 @@ define('TESTING', 'true');
 include_once (__DIR__ . '/../funcionesTesting.php');
 include_once (__DIR__ . '/../../funcionesGenerales.php');
 include_once (__DIR__ . '/../../defines.php');
-include_once ('../../login.php');
+include_once ('../../ABMUsuarios.php');
 
 $eCon=null;
 try{
 $conexion = connect(True);
-ejecutarSQL('login.sql',$conexion);
+ejecutarSQL('alta.sql',$conexion);
 } 
 catch (Exception $eCon) {
     echo 'Excepción capturada: ',  $eCon->getMessage(), "\n";
 }
 if($eCon==null){
-  echo ">> <strong>Login</strong> </br>";
+  echo ">> <strong>Alta Usuario</strong> </br>";
 
   //PRUEBA 1
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "santiago.civetta@comunidad.ub.edu.ar", "password" => "contraseñasantiago"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Gaston", "apellido" => "Bodeman","documento" => "37213234", "fnac" => "1994-09-20","genero" => "Masculino","domicilio" => "Blanco Encalada 4892","telefono" => "45228786","identificador" => "3140","mail" => "gaston.bodeman@comunidad.ub.edu.ar","contraseña" => "contraseñagaston","rol" => "Alumno"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
+  if($datosParseados["errorId"]=='200'){
     echo "Prueba1 OK </br>";
   }
   else{
     echo "Prueba1 FAIL </br>";
   }
-  
+
   //PRUEBA 2
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "mariano.martin@comunidad.ub.edu.ar", "password" => "contraseñasarasa"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Gaston", "apellido" => "Bodeman","documento" => "37213234", "fnac" => "1994-09-20","genero" => "Masculino","domicilio" => "Blanco Encalada 4892","telefono" => "45228786","identificador" => "3140","mail" => "gaston.bodeman@comunidad.ub.edu.ar","contraseña" => "contraseñagaston","rol" => "Alumno"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==credencialesIncorrectas){
+  if($datosParseados["errorId"]==usuarioDuplicado){
     echo "Prueba2 OK </br>";
   }
   else{
@@ -37,9 +37,9 @@ if($eCon==null){
   }
 
   //PRUEBA 3
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "gaston.bodeman@comunidad.ub.edu.ar", "password" => "contraseñagaston"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Santi", "apellido" => "Cive","documento" => "12345678", "fnac" => "1994-09-10","genero" => "Masculino","domicilio" => "Cabildo 321","telefono" => "123456789","identificador" => "3141","mail" => "santiago.civetta@comunidad.ub.edu.ar","contraseña" => "contraseñanueva","rol" => "Alumno"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==personaInexistente){
+  if($datosParseados["errorId"]=='200'){
     echo "Prueba3 OK </br>";
   }
   else{
@@ -47,9 +47,9 @@ if($eCon==null){
   }
 
   //PRUEBA 4
-  $datos= doLogin(["apiVer" => apiVersionActual, "idSesion" => "", "mail" => "andres.didier@comunidad.ub.edu.ar", "password" => "contraseñaandres"]);
+  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "2", "operacion" => "alta", "nombre" => "Santi", "apellido" => "Cive","documento" => "12345678", "fnac" => "1994-09-10","genero" => "Masculino","domicilio" => "Cabildo 321","telefono" => "123456789","identificador" => "3141","mail" => "santiago.civetta@comunidad.ub.edu.ar","contraseña" => "contraseñanueva","rol" => "Alumno"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200' and $datosParseados["usuario"]["idSesion"]>'0'){
+  if($datosParseados["errorId"]==permisosErroneos){
     echo "Prueba4 OK </br>";
   }
   else{
