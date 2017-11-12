32e6b3169376d2cf5772bed7a3a23728485b290b
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-12 15:23:54 -0300

    Se corrigen bugs y se hacen algunas optimizaciones de codigo

--- ABMUsuarios.php
+++ ABMUsuarios.php
@@ -24,9 +24,13 @@ function doABMUsuarios($data) {
 			throw new Exception(permisosErroneos);	
 		}
 
-		$query3='SELECT * FROM Usuarios WHERE usuario="' . $data['mail'] . '"';
+		$query3='SELECT u.*,r.tabla FROM Usuarios u, Roles r WHERE u.idRol=r.idRol and usuario="' . $data['mail'] . '"';
 
 		$arrayUsuario = $conexion->query($query3);
+
+		if ($arrayUsuario->num_rows == 1){
+			$usuario = $arrayUsuario->fetch_array(MYSQLI_ASSOC);
+		}
 
 		if ($conexion->connect_errno) {
 			throw new Exception(errorConexionBase);
@@ -35,19 +39,22 @@ function doABMUsuarios($data) {
 		if (strcmp($data["operacion"], "alta")==0) {
 
 			if ($arrayUsuario->num_rows == 1){
-				$usuario = $arrayUsuario->fetch_array(MYSQLI_ASSOC);
+
 				if($usuario['fechaHasta']==null){
 					throw new Exception(usuarioDuplicado);
 				}else{
+
 					$query4='UPDATE Usuarios set fechaHasta=null WHERE usuario="' . $data['mail'] . '"';
 					if ($conexion->query($query4) === FALSE) {
 						throw new Exception(errorConexionBase);
 					};
 					$data["operacion"]='modificacion';
+
 				}
 			}else{
 
 				if (strcmp($data["rol"],"Administrador")==0) {
+
 					$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),3,null)";
 					
 					$query2 = "INSERT INTO Administradores(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
@@ -55,6 +62,7 @@ function doABMUsuarios($data) {
 				}
 
 				if (strcmp($data["rol"],"Profesor")==0) {
+
 					$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),2,null)";
 
 					$query2 = "INSERT INTO Profesores(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
@@ -62,6 +70,7 @@ function doABMUsuarios($data) {
 				}
 
 				if (strcmp($data["rol"],"Alumno")==0) {
+
 					$query = "INSERT INTO Usuarios(usuario, contraseña, idRol, fechaHasta) values ('".$data["mail"]."',md5('".$data["contraseña"]."'),1,null)";
 
 					$query2 = "INSERT INTO Alumnos(matricula, documento, nombre, apellido, mail, fechaNacimiento, genero, domicilio,telefono) values ('".$data["identificador"]."','".$data["documento"]."','".$data["nombre"]."','".$data["apellido"]."','".$data["mail"]."','".$data["fnac"]."','".$data["genero"]."','".$data["domicilio"]."','".$data["telefono"]."')";
@@ -83,26 +92,10 @@ function doABMUsuarios($data) {
 		if (strcmp($data["operacion"], "modificacion")==0) {
 
 			if ($arrayUsuario->num_rows == 1){
-				if (strcmp($miUsuario["tabla"],"administrador")) {
-					$query = 'UPDATE Administradores SET documento="' . $data['documento'] . '" , nombre="' . $data['nombre'] . '" , apellido="' . $data['apellido'] . '" , fechaNacimiento="' . $data['fnac'] . '" , genero="' . $data['genero'] . '" , domicilio="' . $data['domicilio'] . '" , telefono="' . $data['telefono'] . '" WHERE mail = "' . $data["mail"] . '"';
 
-					$query5 = 'UPDATE Usuarios set contraseña=md5("' . $data['contraseña'] . '") WHERE usuario = "' . $data["mail"] . '"';
+				$query = 'UPDATE ' . $usuario['tabla'] . ' SET documento="' . $data['documento'] . '" , nombre="' . $data['nombre'] . '" , apellido="' . $data['apellido'] . '" , fechaNacimiento="' . $data['fnac'] . '" , genero="' . $data['genero'] . '" , domicilio="' . $data['domicilio'] . '" , telefono="' . $data['telefono'] . '" WHERE mail = "' . $data["mail"] . '"';
 
-				}
-
-				if (strcmp($miUsuario["tabla"],"profesor")) {
-					$query = 'UPDATE Profesores SET documento="' . $data['documento'] . '" , nombre="' . $data['nombre'] . '" , apellido="' . $data['apellido'] . '" , fechaNacimiento="' . $data['fnac'] . '" , genero="' . $data['genero'] . '" , domicilio="' . $data['domicilio'] . '" , telefono="' . $data['telefono'] . '" WHERE mail = "' . $data["mail"] . '"';
-
-					$query5 = 'UPDATE Usuarios set contraseña=md5("' . $data['contraseña'] . '") WHERE usuario = "' . $data["mail"] . '"';
-
-				}
-
-				if (strcmp($miUsuario["tabla"],"alumno")) {
-					$query = 'UPDATE Alumnos SET documento="' . $data['documento'] . '" , nombre="' . $data['nombre'] . '" , apellido="' . $data['apellido'] . '" , fechaNacimiento="' . $data['fnac'] . '" , genero="' . $data['genero'] . '" , domicilio="' . $data['domicilio'] . '" , telefono="' . $data['telefono'] . '" WHERE mail = "' . $data["mail"] . '"';
-
-					$query5 = 'UPDATE Usuarios set contraseña=md5("' . $data['contraseña'] . '") WHERE usuario = "' . $data["mail"] . '"';
-
-				}
+				$query5 = 'UPDATE Usuarios set contraseña=md5("' . $data['contraseña'] . '") WHERE usuario = "' . $data["mail"] . '"';
 
 				if ($conexion->query($query) === FALSE) {
 					throw new Exception(errorConexionBase);
@@ -118,7 +111,9 @@ function doABMUsuarios($data) {
 		}
 
 		if (strcmp($data["operacion"], "baja")==0) {
+
 			if ($arrayUsuario->num_rows == 1){
+
 				$query = '	UPDATE 
 							FROM Usuarios 
 							SET fechaHasta = curdate()
@@ -127,6 +122,7 @@ function doABMUsuarios($data) {
 				if ($conexion->query($query) === FALSE) {
 					throw new Exception(errorConexionBase);
 				};
+
 			}else{
 				throw new Exception(usuarioInexistente);
 			}
