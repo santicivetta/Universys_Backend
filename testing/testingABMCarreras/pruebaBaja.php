166fea6d8375d94fe52744f101bf25bd735cfd46
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-14 00:27:32 -0300

    wip pruebabaja, hay que arreglar un update de baja

--- testing/testingABMCarreras/pruebaBaja.php
+++ testing/testingABMCarreras/pruebaBaja.php
@@ -3,34 +3,34 @@ define('TESTING', 'true');
 include_once (__DIR__ . '/../funcionesTesting.php');
 include_once (__DIR__ . '/../../funcionesGenerales.php');
 include_once (__DIR__ . '/../../defines.php');
-include_once ('../../ABMUsuarios.php');
+include_once ('../../ABMCarreras.php');
 
 $eCon=null;
 try{
-  $conexion = connect(True);
-  ejecutarSQL('baja.sql',$conexion);
+$conexion = connect(True);
+ejecutarSQL('baja.sql',$conexion);
 } 
 catch (Exception $eCon) {
     echo 'Excepción capturada: ',  $eCon->getMessage(), "\n";
 }
-
 if($eCon==null){
-  echo ">> <strong>Baja Usuario</strong> </br>";
+  echo ">> <strong>Baja Carreras</strong> </br>";
 
   //PRUEBA 1
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "mail" => "gaston.bodeman@comunidad.ub.edu.ar"]);
-  $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==usuarioInexistente){
+  $datos= doABMCarreras(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "idCarrera" => "1"]);
+  #$datosParseados= json_decode($datos, true);
+  var_dump($datos);
+  if($datosParseados["errorId"]=='200'){
     echo "Prueba1 OK </br>";
   }
   else{
     echo "Prueba1 FAIL </br>";
   }
-
+/*
   //PRUEBA 2
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "baja", "mail" => "andres.didier@comunidad.ub.edu.ar"]);
+  $datos= doABMCarreras(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "modificacion", "carrera" => "Tecnicatura en Program", "idCarrera" => "5"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200'){
+  if($datosParseados["errorId"]==carreraInexistente){
     echo "Prueba2 OK </br>";
   }
   else{
@@ -38,16 +38,24 @@ if($eCon==null){
   }
 
   //PRUEBA 3
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "2", "operacion" => "baja", "mail" => "sarasa@comunidad.ub.edu.ar"]);
+  $datos= doABMCarreras(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "modificacion", "carrera" => "Tecnicatura en Progra", "idCarrera" => "5"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==permisosErroneos){
+  if($datosParseados["errorId"]==carreraDuplicada){
     echo "Prueba3 OK </br>";
   }
   else{
     echo "Prueba3 FAIL </br>";
-  }
+  }*/
 
 }
 mysqli_close($conexion);
 
 ?>
+
+
+
+
+
+
+
+
