d606ea5e25a56d916c9fe52cb0c799e2e9c48374
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-14 00:25:34 -0300

    se arma .sql para alta

--- testing/testingABMCarreras/alta.sql
+++ testing/testingABMCarreras/alta.sql
@@ -6,19 +6,12 @@ INSERT INTO Roles(descripcion,tabla) values
 
 TRUNCATE TABLE Usuarios;
 INSERT INTO Usuarios values
-("bedel.gato@comunidad.ub.edu.ar",md5("contraseñabedel"),3,null),
-("santiago.civetta@comunidad.ub.edu.ar",md5("contraseñasantiago"),1,curdate());
+("bedel.gato@comunidad.ub.edu.ar",md5("contraseñabedel"),3,null);
 
 TRUNCATE TABLE Sesiones;
 INSERT INTO Sesiones(usuario,fechaAlta) values
-("bedel.gato@comunidad.ub.edu.ar",CURDATE()),
-("santiago.civetta@comunidad.ub.edu.ar",curdate());
+("bedel.gato@comunidad.ub.edu.ar",CURDATE());
 
-
-TRUNCATE TABLE Administradores;
-
-TRUNCATE TABLE Profesores;
-
-TRUNCATE TABLE Alumnos;
-INSERT INTO Alumnos values
-("3141","32341231","Santiago","Civetta","santiago.civetta@comunidad.ub.edu.ar","1990-03-02","Masculino","cabildo 123","47134567");
+TRUNCATE TABLE Carreras;
+INSERT INTO Carreras(nombre,fechaHasta) value
+("Tecnicatura en Programacion",curdate());
