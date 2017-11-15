505e09c3a9d62083a173c89be397480c3c733d20
Unknown <santiago.civettam@gmail.com>
2017-11-15 10:18:11 -0300

    Subo pruebas para modificacion en ABM Catedras

---
+++ testing/testingAbmCatedras/modificacion.sql
@@ -0,0 +1,20 @@
+TRUNCATE TABLE Roles;
+INSERT INTO Roles(descripcion,tabla) values
+("Alumno","Alumnos"),
+("Profesor","Profesores"),
+("Administrador","Administradores");
+
+TRUNCATE TABLE Usuarios;
+INSERT INTO Usuarios values
+("bedel.gato@comunidad.ub.edu.ar",md5("contraseñabedel"),3,null);
+
+TRUNCATE TABLE Sesiones;
+INSERT INTO Sesiones(usuario,fechaAlta) values
+("bedel.gato@comunidad.ub.edu.ar",CURDATE());
+
+INSERT INTO Materias(nombre) values
+("Testing");
+
+
+TRUNCATE TABLE Catedras;
+INSERT INTO Catedras(idMateria, catedra, fechaHasta) values ('1','Marmol', curdate());
