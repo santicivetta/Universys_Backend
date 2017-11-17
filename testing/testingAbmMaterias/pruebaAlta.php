f7d64c87d67acd86fcb3fac9f5b6f23220c713cf
Mariano Martin <mariano.d.martin@gmail.com>
2017-11-17 00:02:02 -0300

    se realizan 3 testings para alta

--- testing/testingAbmMaterias/pruebaAlta.php
+++ testing/testingAbmMaterias/pruebaAlta.php
@@ -3,7 +3,7 @@ define('TESTING', 'true');
 include_once (__DIR__ . '/../funcionesTesting.php');
 include_once (__DIR__ . '/../../funcionesGenerales.php');
 include_once (__DIR__ . '/../../defines.php');
-include_once ('../../ABMUsuarios.php');
+include_once ('../../ABMMaterias.php');
 
 $eCon=null;
 try{
@@ -17,9 +17,9 @@ if($eCon==null){
   echo ">> <strong>Alta Materia</strong> </br>";
 
   //PRUEBA 1
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Gaston", "apellido" => "Bodeman","documento" => "37213234", "fnac" => "1994-09-20","genero" => "Masculino","domicilio" => "Blanco Encalada 4892","telefono" => "45228786","identificador" => "3140","mail" => "gaston.bodeman@comunidad.ub.edu.ar","contraseña" => "contraseñagaston","rol" => "Alumno"]);
+  $datos= doABMMaterias(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "materia" => "Analisis Matematico"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200'){
+  if($datosParseados["errorId"]==salidaExitosa){
     echo "Prueba1 OK </br>";
   }
   else{
@@ -27,9 +27,9 @@ if($eCon==null){
   }
 
   //PRUEBA 2
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Gaston", "apellido" => "Bodeman","documento" => "37213234", "fnac" => "1994-09-20","genero" => "Masculino","domicilio" => "Blanco Encalada 4892","telefono" => "45228786","identificador" => "3140","mail" => "gaston.bodeman@comunidad.ub.edu.ar","contraseña" => "contraseñagaston","rol" => "Alumno"]);
+  $datos= doABMMaterias(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "materia" => "Analisis Matematico"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==usuarioDuplicado){
+  if($datosParseados["errorId"]==materiaDuplicada){
     echo "Prueba2 OK </br>";
   }
   else{
@@ -37,24 +37,15 @@ if($eCon==null){
   }
 
   //PRUEBA 3
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "nombre" => "Santi", "apellido" => "Cive","documento" => "12345678", "fnac" => "1994-09-10","genero" => "Masculino","domicilio" => "Cabildo 321","telefono" => "123456789","identificador" => "3141","mail" => "santiago.civetta@comunidad.ub.edu.ar","contraseña" => "contraseñanueva","rol" => "Alumno"]);
+  $datos= doABMMaterias(["apiVer" => apiVersionActual, "idSesion" => "1", "operacion" => "alta", "materia" => "Algebra"]);
   $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]=='200'){
+  if($datosParseados["errorId"]==salidaExitosa){
     echo "Prueba3 OK </br>";
   }
   else{
     echo "Prueba3 FAIL </br>";
   }
 
-  //PRUEBA 4
-  $datos= doABMUsuarios(["apiVer" => apiVersionActual, "idSesion" => "2", "operacion" => "alta", "nombre" => "Santi", "apellido" => "Cive","documento" => "12345678", "fnac" => "1994-09-10","genero" => "Masculino","domicilio" => "Cabildo 321","telefono" => "123456789","identificador" => "3141","mail" => "santiago.civetta@comunidad.ub.edu.ar","contraseña" => "contraseñanueva","rol" => "Alumno"]);
-  $datosParseados= json_decode($datos, true);
-  if($datosParseados["errorId"]==permisosErroneos){
-    echo "Prueba4 OK </br>";
-  }
-  else{
-    echo "Prueba4 FAIL </br>";
-  }
 }
 mysqli_close($conexion);
 
